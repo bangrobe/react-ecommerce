b4b6d39b1f990f2b1bdbfeb544d7b37c57062e2c
bangrobe <bangrobe@gmail.com>
2022-09-25 14:54:42 +0700

    Add sort features

--- src/context/filter_context.jsx
+++ src/context/filter_context.jsx
@@ -15,7 +15,8 @@ import { useProductsContext } from './products_context'
 const initialState = {
   filtered_products: [],
   all_products: [],
-  grid_view: false
+  grid_view: false,
+  sort: 'price-lowest' //Currently has 4 value: price-lowest, price-highest, name-a-z, name-z-a
 }
 
 const FilterContext = React.createContext()
@@ -29,8 +30,29 @@ export const FilterProvider = ({ children }) => {
     //dispatch action to filter_reducer
     dispatch({type: LOAD_PRODUCTS, payload:products})
   },[products])
+
+  //set grid view or list view
+  const setGridView = () => {
+    dispatch({type: SET_GRIDVIEW})
+  }
+
+  const setListView = () => {
+    dispatch({type: SET_LISTVIEW})
+  }
+
+  const updateSort = (e) => {
+
+    // const name = e.target.name;
+    const value = e.target.value;
+    dispatch({type: UPDATE_SORT, payload: value})
+  }
+
+  //useEffect dispatch SORT_PRODUCTS in reducer after running updateSort
+  useEffect(()=> {
+    dispatch({type: SORT_PRODUCTS})
+  },[products, state.sort])
   return (
-    <FilterContext.Provider value={{...state}}>
+    <FilterContext.Provider value={{...state, setGridView, setListView, updateSort }}>
       {children}
     </FilterContext.Provider>
   )
--- src/reducers/filter_reducer.jsx
+++ src/reducers/filter_reducer.jsx
@@ -15,6 +15,41 @@ import {
       // Dùng spread operator copy ra một mảng không ảnh hưởng
       return {...state, all_products: [...action.payload], filtered_products: [...action.payload]}
     }
+
+    //Set grid_view value in filter_context
+    if (action.type === SET_GRIDVIEW) {
+      return { ...state, grid_view:true}
+    }
+    if (action.type === SET_LISTVIEW) {
+      return { ...state, grid_view: false }
+    }
+    //Update sort action from sort.js component, state in filter_context
+    if (action.type === UPDATE_SORT) {
+      console.log("In reducer", action.payload)
+      return { ...state, sort: action.payload}
+    }
+
+    if (action.type === SORT_PRODUCTS) {
+      const { sort, filtered_products } = state;
+      let tempProducts = [...filtered_products];
+      if (sort === 'price-lowest') {
+        tempProducts = tempProducts.sort((a, b)=> a.price - b.price)
+      }
+      if (sort === 'price-highest') {
+        tempProducts = tempProducts.sort((a, b)=> b.price - a.price)
+      }
+      if (sort === 'name-a-z') {
+        tempProducts = tempProducts.sort((a, b)=> {
+          return a.name.localeCompare(b.name)
+        })
+      }
+      if (sort === 'name-z-a') {
+        tempProducts = tempProducts.sort((a, b)=> {
+          return b.name.localeCompare(a.name)
+        })
+      }
+      return { ...state, filtered_products: tempProducts}
+    }
     throw new Error(`No Matching "${action.type}" - action type`)
   }
   
